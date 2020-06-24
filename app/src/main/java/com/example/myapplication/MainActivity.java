26a1a84856824ab1169ceebbf7f9bcc16e778be5
Pro <MishaKylik.1909>
2020-06-24 17:45:36 +0300

    k1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -62,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 searchGroup();
+
             }
         });
         newlist = new ArrayList<>();
